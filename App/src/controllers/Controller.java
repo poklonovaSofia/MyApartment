222c29933ebc945547738a3e51b62c91f211824e
Sofia <sofapokl@gmail.com>
2024-03-19 10:43:57 +0100

    App :: Init

---
+++ App/src/controllers/Controller.java
@@ -0,0 +1,51 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private Button buttonSignIn;
+
+    @FXML
+    private Button buttonSignUp;
+
+    @FXML
+    private void handleButtonClicked(ActionEvent event) {
+        if (event.getSource() == buttonSignIn) {
+            loadStage("views/SignIn.fxml");
+        } else if (event.getSource() == buttonSignUp) {
+            loadStage("views/SignUp.fxml");
+        }
+    }
+    private void loadStage(String fxml) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(fxml));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            //stage.getIcons().add(new Image("/home/icons/icon.png"));
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
